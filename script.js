955a35ac2e4d74982ae2c471639a80521b603545
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-25 20:46:06 -0400

    added timer code to the project

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
     // Get current date and time
     const currentDate = new Date();
     const currentHour = currentDate.getHours();
+     // Display the current date in the header
+     const currentDayElement = document.getElementById("currentDay");
+     currentDayElement.textContent = currentDate.toLocaleDateString();
+ 
+     // Generate time blocks
+     const timeBlocksElement = document.getElementById("timeBlocks");
+     for (let hour = 9; hour <= 17; hour++) {
+        const timeBlock = document.createElement("div");
+        timeBlock.classList.add("time-block");
+
+        // Color code time blocks based on past, present, or future
+        if (hour < currentHour) {
+            timeBlock.classList.add("past");
+        } else if (hour === currentHour) {
+            timeBlock.classList.add("present");
+        } else {
+            timeBlock.classList.add("future");
+        }
+ 
