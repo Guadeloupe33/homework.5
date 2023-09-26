fcacbcdf067eeed8f5072bd20f5d2a35bd76fe6e
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-25 22:02:12 -0400

    made sveral changes to the functionality of the page

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get current date and time
+ 
     const currentDate = new Date();
     const currentHour = currentDate.getHours();
-     // Display the current date in the header
-     const currentDayElement = document.getElementById("currentDay");
-     currentDayElement.textContent = currentDate.toLocaleDateString();
- 
-     // Generate time blocks
-     const timeBlocksElement = document.getElementById("timeBlocks");
-     for (let hour = 9; hour <= 17; hour++) {
+    const isAM = currentHour < 12;
+    const amPM = isAM ? "AM" : "PM";
+
+    
+    const currentDayElement = document.getElementById("currentDay");
+    currentDayElement.textContent = currentDate.toLocaleDateString() + " " + currentHour + ":00 " + amPM;
+
+   
+    const timeBlocksElement = document.getElementById("timeBlocks");
+
+    for (let hour = 9; hour <= 17; hour++) {
         const timeBlock = document.createElement("div");
         timeBlock.classList.add("time-block");
 
-        // Color code time blocks based on past, present, or future
+      
         if (hour < currentHour) {
             timeBlock.classList.add("past");
         } else if (hour === currentHour) {
-            timeBlock.classList.add( "present");
+            timeBlock.classList.add("present");
         } else {
             timeBlock.classList.add("future");
         }
+
+       
+        const displayHour = hour > 12 ? hour - 12 : hour;
+        const amPM = hour >= 12 ? "PM" : "AM";
+
         const hourLabel = document.createElement("div");
-        hourLabel.textContent = `${hour}:00`;
+        hourLabel.textContent = `${displayHour}:00 ${amPM}`;
 
         const eventInput = document.createElement("textarea");
         eventInput.setAttribute("data-hour", hour);
 
         const saveButton = document.createElement("button");
         saveButton.textContent = "Save";
+
+     
         const savedEvent = localStorage.getItem(`event_${hour}`);
         if (savedEvent) {
             eventInput.value = savedEvent;
         }
 
-        // Save event to local storage when the "Save" button is clicked
+      
         saveButton.addEventListener("click", function () {
             const eventText = eventInput.value;
             localStorage.setItem(`event_${hour}`, eventText);
